52ac1bfd2c302c209e4de916ed222f39e04aee7c
和錦わきん <156275155+wakin721@users.noreply.github.com>
2025-07-01 20:57:43 +0800

    Update config.py

--- system/config.py
+++ system/config.py
@@ -3,8 +3,8 @@
 """
 
 # 应用信息常量
-APP_TITLE = "物种信息检测 v2.2-alpha"
-APP_VERSION = "2.2.3-alpha"
+APP_TITLE = "物种信息检测 v2.2-beta"
+APP_VERSION = "2.2.3-beta"
 DEFAULT_EXCEL_FILENAME = "物种检测信息.xlsx"
 
 # 文件支持相关常量
