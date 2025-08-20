ff7a46fb1eb5ab26b38331b23e0adc68c6bba840
和錦わきん <156275155+wakin721@users.noreply.github.com>
2025-08-20 09:36:27 +0800

    Update config.py

--- system/config.py
+++ system/config.py
@@ -4,7 +4,7 @@
 
 # 应用信息常量
 APP_TITLE = "物种信息检测 v2.5"
-APP_VERSION = "2.5.5-beta"
+APP_VERSION = "2.5.5-release"
 DEFAULT_EXCEL_FILENAME = "物种检测信息.xlsx"
 
 # 文件支持相关常量
@@ -18,3 +18,4 @@ BUTTON_WIDTH = 14
 LARGE_FONT = ('Segoe UI', 11)
 NORMAL_FONT = ('Segoe UI', 10)
 SMALL_FONT = ('Segoe UI', 9)
+
